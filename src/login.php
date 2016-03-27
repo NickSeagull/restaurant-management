a4933aaf7231c9bcf284791fb4a16f96842b696b
Nick Tchayka <nikitatchayka@gmail.com>
2016-03-27 17:18:48 +0100

    Remove unnecesary lines

--- src/login.php
+++ src/login.php
@@ -1,9 +1,5 @@
 <?php
 require_once("View.php");
-require_once("ContentExtractor.php");
-
-$content_extractor = new ContentExtractor();
-$content = $content_extractor->get();
 
 $view = new View();
 $view->render('login');
